3e2e32c3249abb871b6c9ae9930cb726c70e96bf
naive <to.naive@gmail.com>
2024-09-09 05:00:10 +0300

    +area

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ window.onload = () => {
   // https://www.somesolvedproblems.com/2018/07/how-do-i-make-plotly-faster.html?m=1
   const plot = document.getElementById('plot')
   let type = (new URLSearchParams(window.location.search)).get('type')
-  let area = (new URLSearchParams(window.location.search)).get('area') || 'athens'
+  const area = (new URLSearchParams(window.location.search)).get('area') || 'athens'
   type = ['sale', 'rent', 'roi', 'area'].includes(type) ? type : 'sale'
   const config_all = [
     {
@@ -11,7 +11,8 @@ window.onload = () => {
       cmax: 7000,
       cmin: 1000,
       title: '€/m2',
-      size: 10
+      size: 10,
+      hovertemplate: null
     },
     {
       type: 'rent',
@@ -19,7 +20,8 @@ window.onload = () => {
       cmax: 20,
       cmin: 5,
       title: '€/m2',
-      size: 10
+      size: 10,
+      hovertemplate: null
     },
     {
       type: 'roi',
@@ -27,7 +29,17 @@ window.onload = () => {
       cmax: 5,
       cmin: 2,
       title: '%/year',
-      size: 30
+      size: 30,
+      hovertemplate: null
+    },
+    {
+      type: 'area',
+      z_key: 'sale_price_per_meter',
+      cmax: 7000,
+      cmin: 1000,
+      title: '€/m2',
+      size: (r) => 10 + r.n_sale / 10,
+      hovertemplate: '</br><b>%{customdata.geography}</b></br>On Sale: %{customdata.n_sale}</br>On Rent: %{customdata.n_rent}</br>Sale €/m2: %{customdata.sale_price_per_meter}</br>Rent €/m2: %{customdata.rent_price_per_meter}</br>ROI: %{customdata.ROI}<extra></extra>'
     }
   ]
   const config = config_all.find(x => x.type === type) || config_all.find(x => x.type === 'default')
@@ -55,14 +67,16 @@ window.onload = () => {
       z: rows.map(r => r[config.z_key]),
       type: 'scattermap',
       coloraxis: 'coloraxis',
+      customdata: config.hovertemplate ? rows : null,
       hoverinfo: 'text',
       hovertext: rows.map(r => r[config.z_key]),
+      hovertemplate: config.hovertemplate || null,
       marker: {
         allowoverlap: true,
         colorscale: 'Viridis', // Viridis,Jet,Reds,Earth,Rainbow,[[0, 'rgb(100,100,100)'], [1, 'rgb(255,0,0)']]
         color: rows.map(r => r[config.z_key]),
         opacity: 0.8,
-        size: config.size,
+        size: typeof config.size === 'function' ? rows.map(config.size) : config.size || 10,
         // sizemin: 4,
         // size: rows.map(r => r.rooms > 4 ? 16 : r.rooms * 4),
         cmax: config.cmax,
@@ -76,7 +90,31 @@ window.onload = () => {
       connectgaps: false
     }]
 
+    /*
+    const template = {
+      data: {},
+      layout: {
+        title: 'Watermark',
+        images: [
+          {
+            name: 'watermark',
+            // source: 'https://raw.githubusercontent.com/michaelbabyn/plot_data/master/naphthalene.png',
+            xref: 'paper',
+            yref: 'paper',
+            x: 0.75,
+            y: 0.3,
+            sizex: 0.25,
+            sizey: 0.25,
+            sizing: 'stretch',
+            opacity: 0.1,
+            layer: 'over'
+          }],
+        showlegend: false
+      }
+    }
+    */
     const layout = {
+      // template,
       dragmode: 'zoom',
       map: {
         center: { lon: 23.75, lat: 38 },
@@ -101,9 +139,11 @@ window.onload = () => {
       // console.log(eventdata)
       const new_zoom = parseInt(eventdata['map.zoom'])
       if (last_zoom !== new_zoom) {
-        console.log('Zoom changed: %s -> %s [%s]', last_zoom, new_zoom, first_marker_size * new_zoom / first_zoom)
+        console.log('Zoom changed: %s -> %s [%s]', last_zoom, new_zoom, new_zoom / first_zoom)
         last_zoom = new_zoom
-        Plotly.update(plot, { 'marker.size': first_marker_size * new_zoom / first_zoom })
+        const new_marker_size = typeof first_marker_size === 'object' ? first_marker_size.map(r => Math.round(r * new_zoom / first_zoom)) : first_marker_size * new_zoom / first_zoom
+        console.log(new_marker_size)
+        Plotly.update(plot, [{ 'marker.size': new_marker_size }])
       }
     })
   })
--- roi.sql
+++ roi.sql
@@ -1,4 +1,4 @@
-SELECT MEDIAN(CAST(longitude  as decimal)) AS Long, MEDIAN(CAST(latitude  as decimal)) AS Lat, ROUND(100 * 12 * (SELECT MEDIAN(IFNULL(price,0)/sq_meters) FROM spitogatos.properties rent WHERE rent.type='rent' AND rent.geography=sale.geography)/MEDIAN(IFNULL(sale.price,0)/sale.sq_meters), 1) AS ROI
+SELECT MEDIAN(CAST(longitude  as decimal)) AS Long, MEDIAN(CAST(latitude  as decimal)) AS Lat, ROUND(100 * 12 * (SELECT MEDIAN(IFNULL(price,0)/sq_meters) FROM spitogatos.properties rent WHERE rent.type='rent' AND rent.geography=sale.geography AND imported_at IS NOT NULL AND modified > '2024-01-01')/MEDIAN(IFNULL(sale.price,0)/sale.sq_meters), 1) AS ROI
 FROM spitogatos.properties sale
 WHERE sale.type='sale' AND imported_at IS NOT NULL AND modified > '2024-01-01'
 GROUP BY sale.geography
