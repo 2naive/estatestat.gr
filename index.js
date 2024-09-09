2dfb9cb3c25477ecc36e2926c49b2cfb48fdf4a1
naive <to.naive@gmail.com>
2024-09-09 05:36:24 +0300

    fix: area size

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ window.onload = () => {
       cmax: 6000,
       cmin: 1000,
       title: '€/m2',
-      size: (r) => 20 + r.n_sale / 20,
+      size: (r) => 10 + r.n_sale / 15,
       hovertemplate: '</br><b>%{customdata.geography}</b></br>On Sale: %{customdata.n_sale}</br>On Rent: %{customdata.n_rent}</br>Sale €/m2: %{customdata.sale_price_per_meter}</br>Rent €/m2: %{customdata.rent_price_per_meter}</br>ROI: %{customdata.ROI}<extra></extra>'
     }
   ]
